a355e43e0018a3e8498d33b8e8b153567d76d692
marcuslouis <Louis.Marcus@live.jobcorps.org>
2022-05-09 14:31:40 +0000

    starApi

--- src/main.py
+++ src/main.py
@@ -46,10 +46,21 @@ def handle_onePerson():
 @app.route('/people', methods=['POST'])
 def create_people():
     request_body = request.get_json()
-    new_people = People(id=request_body['id'],email=request_body['email'], password=request_body['password'], is_active=request_body['is_active'])
+    new_people = People(email=request_body['email'], password=request_body['password'], is_active=request_body['is_active'])
     db.session.add(new_people)
     db.session.commit()
     return f"The new people {request_body['email']} was created successfully", 200
+
+
+@app.route('/people', methods=['DELETE'])
+def delete_people():
+    request_body = request.get_json()
+    new_people = People(email=request_body['email'], password=request_body['password'], is_active=request_body['is_active'])
+    db.session.add(new_people)
+    db.session.commit()
+     return f"The new people {request_body['email']} was deleted successfully", 200
+
+
 
 
 @app.route('/planets', methods=['GET'])
@@ -74,12 +85,28 @@ def create_planets():
     db.session.commit()
     return f"The new planet {request_body['planet_name']} was created successfully", 200
 
+
+@app.route('/planets', methods=['DELETE'])
+def create_planets():
+    request_body = request.get_json()
+    new_planets = Planets(planet_name=request_body['planet_name'], diameter=request_body['diameter'], rotation_period=request_body['rotation_period'],gravity=request_body['gravity'], population=request_body['population'], climate=request_body['climate'],terrian=request_body['terrian'], surface_water=request_body['surface_water'])
+    db.session.add(new_planets)
+    db.session.commit()
+    return f"The new planet {request_body['planet_name']} was deleted successfully", 200
+
 @app.route('/characters', methods=['GET'])
 def handle_characters():
 
     characters  = Characters.query.all()
     character_list = list(map(lambda x: x.serialize(),characters))
     return jsonify(character_list), 200
+
+
+@app.route('/characters/<int:character_id>', methods=['GET'])
+def handle_onecharacter():
+    character = Characters.query.get(character_id)
+    return jsonify(character.serialize)
+
 
 @app.route('/characters', methods=['POST'])
 def create_characters():
@@ -89,23 +116,13 @@ def create_characters():
     db.session.commit()
     return f"The new character {request_body['Characters_name']} was created successfully", 200
 
-@app.route('/favorites/planets/<int:planets_id>', methods=['POST'])
-def handle_favorites_planets():
-    planets = Planets.query.get(planets_id)
-    return jsonify(planets.serialize)
-
-@app.route('/favorites/people/<int:people_id>', methods=['DELETE'])
-def handle_delete(people_id):
-
-    people = People.query.get(people_id)
-    if people is None:
-        raise APIException('User not found', status_code=404)
-    db.session.delete(people)
+@app.route('/characters', methods=['DELETE'])
+def create_characters():
+    request_body = request.get_json()
+    new_character = Characters(Characters_name=request_body['Characters_name'], gender=request_body['gender'], movies=request_body['movies'],eye_color=request_body['eye_color'], height=request_body['height'], hair_color=request_body['hair_color'], birth_year=request_body['birth_year'], mass=request_body['mass'], homeworld=request_body['homeworld'])
+    db.session.add(new_characters)
     db.session.commit()
-    return jsonify(people_id)
-
-
-
+    return f"The new character {request_body['Characters_name']} was deleted successfully", 200
 
 
 # this only runs if `$ python src/main.py` is executed
